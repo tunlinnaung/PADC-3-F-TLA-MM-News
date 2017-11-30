f1a320ad720ee5b806704de00a75432c1bc3c5d0
tunlinaung <tunlinaung.java@gmail.com>
2017-11-30 21:40:13 +0630

    Add butterknife binding

--- app/src/main/java/xyz/tunlinaung/news/activities/MainActivity.java
+++ app/src/main/java/xyz/tunlinaung/news/activities/MainActivity.java
@@ -4,30 +4,33 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import xyz.tunlinaung.news.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.rv_news) RecyclerView rvNews;
+
+    @BindView(R.id.toolbar) Toolbar toolbar;
+
+    @BindView(R.id.fab) FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        ButterKnife.bind(this,this);
+
+        setSupportActionBar(toolbar);
     }
 
     @Override
@@ -50,5 +53,11 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @OnClick(R.id.fab)
+    public void onTabFab(View view) {
+        Snackbar.make(view, "Replace with your own action with ButterKnife", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show();
     }
 }
