f0fbb6cd5c6e89d19d500588b99ea05ebfce0e23
tunlinaung <tunlinaung.java@gmail.com>
2017-11-30 20:50:11 +0630

    Project initialize - xml layout - package - application - theme and color

---
+++ app/src/main/java/xyz/tunlinaung/news/MMNewsApp.java
@@ -0,0 +1,10 @@
+package xyz.tunlinaung.news;
+
+import android.app.Application;
+
+/**
+ * Created by eidoshack on 11/29/17.
+ */
+
+public class MMNewsApp extends Application {
+}
--- app/src/main/java/xyz/tunlinaung/news/activities/MainActivity.java
+++ app/src/main/java/xyz/tunlinaung/news/activities/MainActivity.java
@@ -1,4 +1,4 @@
-package xyz.tunlinaung.padc_3_f_tla_mm_news;
+package xyz.tunlinaung.news.activities;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -8,6 +8,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import xyz.tunlinaung.news.R;
 
 public class MainActivity extends AppCompatActivity {
 
