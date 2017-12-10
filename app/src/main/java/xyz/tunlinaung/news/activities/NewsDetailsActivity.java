59725e9b8f8a76f602a9d5fef894f5468d316d6a
tunlinaung <tunlinaung.java@gmail.com>
2017-12-10 15:17:40 +0630

    implement view pager for news details images

--- app/src/main/java/xyz/tunlinaung/news/activities/NewsDetailsActivity.java
+++ app/src/main/java/xyz/tunlinaung/news/activities/NewsDetailsActivity.java
@@ -2,9 +2,15 @@ package xyz.tunlinaung.news.activities;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.Toolbar;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import xyz.tunlinaung.news.R;
+import xyz.tunlinaung.news.adapters.ImagesInNewsDetailsAdapter;
 
 /**
  * Created by eidoshack on 12/9/17.
@@ -12,11 +18,27 @@ import xyz.tunlinaung.news.R;
 
 public class NewsDetailsActivity extends AppCompatActivity {
 
+    @BindView(R.id.toolbar_details) Toolbar toolbar;
+
+    @BindView(R.id.vp_news_details_images) ViewPager viewPager;
+
+    private ImagesInNewsDetailsAdapter mImagesInNewsDetailsAdapter;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news_details);
+
+        ButterKnife.bind(this, this);
+
+        //setSupportActionBar(toolbar);
+
+        // hide toolbar's title
+        //getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        mImagesInNewsDetailsAdapter = new ImagesInNewsDetailsAdapter();
+        viewPager.setAdapter(mImagesInNewsDetailsAdapter);
 
     }
 }
---
+++ app/src/main/java/xyz/tunlinaung/news/adapters/ImagesInNewsDetailsAdapter.java
@@ -0,0 +1,57 @@
+package xyz.tunlinaung.news.adapters;
+
+import android.content.Context;
+import android.support.v4.view.PagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import viewitems.ImageInNewsDetailsViewItem;
+import xyz.tunlinaung.news.R;
+
+/**
+ * Created by eidoshack on 12/10/17.
+ */
+
+public class ImagesInNewsDetailsAdapter extends PagerAdapter {
+
+    @Override
+    public int getCount()
+    {
+        return 6;
+    }
+
+    /* check that object is view type or not*/
+    @Override
+    public boolean isViewFromObject(View view, Object object)
+    {
+//        if (object instanceof View)
+//            return true;
+//        else
+//            return false;
+
+//        return (object instanceof View);
+
+        return (view == (View) object);
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position)
+    {
+        Context context = container.getContext();
+        LayoutInflater layoutInflater = LayoutInflater.from(context);
+        ImageInNewsDetailsViewItem view =
+                (ImageInNewsDetailsViewItem) layoutInflater.inflate(R.layout.item_news_details_images,
+                                                                    container, false);
+        // manually put view to viewGroup (only for ViewPager)
+        container.addView(view);
+        return view;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object)
+    {
+        container.removeView((View) object);
+
+    }
+}
